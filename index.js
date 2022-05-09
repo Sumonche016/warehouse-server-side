d8941ca4aaa4af9b0dcd9aa2dccfde2f511a4cb5
Sumonche016 <sumonche016@gmail.com>
2022-05-09 14:25:11 +0600

    deleted updated

--- index.js
+++ index.js
@@ -39,6 +39,15 @@ async function run() {
             res.send(service)
         })
 
+        // delete api 
+
+        app.delete('/manage-inventory/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+
     }
 
     finally {
